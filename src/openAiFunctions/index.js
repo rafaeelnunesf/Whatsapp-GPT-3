a331ed34c4e3b85c8ed69fec50742b1f7f6cbd40
Rafael Nunes <rafaelnfssq@gmail.com>
2023-01-26 08:48:34 -0300

    feat: create openAI response functions

---
+++ src/openAiFunctions/index.js
@@ -0,0 +1,5 @@
+import { getDalleResponse } from "./getDalleResponse.js";
+import { getDavinciResponse } from "./getDavinciResponse.js";
+import { commands } from "./commands.js";
+
+export { getDalleResponse, getDavinciResponse, commands };
